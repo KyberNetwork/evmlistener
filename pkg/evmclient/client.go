e59b72a8cbf3c99642342a8210bdda7afa9e4177
Hiep <hiep.nguyen@kyber.network>
2024-11-08 16:32:46 +0700

    Implement simple client for avax and upgrade go-ethereum

--- pkg/evmclient/client.go
+++ pkg/evmclient/client.go
@@ -8,13 +8,9 @@ import (
 	"time"
 
 	"github.com/KyberNetwork/evmlistener/pkg/common"
-	"github.com/KyberNetwork/evmlistener/pkg/evmclient/ftmclient"
-	zksyncclient "github.com/KyberNetwork/evmlistener/pkg/evmclient/zksync-client"
+	"github.com/KyberNetwork/evmlistener/pkg/evmclient/avax"
+	commonclient "github.com/KyberNetwork/evmlistener/pkg/evmclient/common"
 	"github.com/KyberNetwork/evmlistener/pkg/types"
-	avaxtypes "github.com/ava-labs/coreth/core/types"
-	aethclient "github.com/ava-labs/coreth/ethclient"
-	"github.com/ava-labs/coreth/interfaces"
-	"github.com/ethereum/go-ethereum"
 	ethcommon "github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -58,9 +54,8 @@ type IClient interface {
 type Client struct {
 	chainID      uint64
 	ethClient    *ethclient.Client
-	ftmClient    *ftmclient.Client
-	avaxClient   aethclient.Client
-	zksyncClient *zksyncclient.Client
+	customClient *commonclient.Client
+	avaxClient   *avax.Client
 }
 
 func Dial(rawurl string, httpClient *http.Client) (*Client, error) {
@@ -84,12 +79,10 @@ func DialContext(ctx context.Context, rawurl string, httpClient *http.Client) (*
 	}
 
 	switch client.chainID {
-	case chainIDFantom:
-		client.ftmClient = ftmclient.NewClient(rpcClient)
+	case chainIDFantom, chainIDZKSync:
+		client.customClient = commonclient.NewClient(rpcClient)
 	case chainIDAvalanche:
-		client.avaxClient, err = AvaxDialContext(ctx, rawurl, httpClient)
-	case chainIDZKSync:
-		client.zksyncClient = zksyncclient.NewClient(rpcClient)
+		client.avaxClient = avax.NewClient(rpcClient)
 	default:
 		client.ethClient = ethClient
 	}
@@ -137,71 +130,30 @@ func (c *Client) ChainID(ctx context.Context) (*big.Int, error) {
 
 func (c *Client) BlockNumber(ctx context.Context) (uint64, error) {
 	switch c.chainID {
-	case chainIDFantom:
-		return c.ftmClient.BlockNumber(ctx)
+	case chainIDFantom, chainIDZKSync:
+		return c.customClient.BlockNumber(ctx)
 	case chainIDAvalanche:
 		return c.avaxClient.BlockNumber(ctx)
-	case chainIDZKSync:
-		return c.zksyncClient.BlockNumber(ctx)
 	default:
 		return c.ethClient.BlockNumber(ctx)
 	}
 }
 
-//nolint:cyclop,ireturn,funlen,gocognit
+//nolint:cyclop,ireturn,gocognit
 func (c *Client) SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (Subscription, error) {
 	switch c.chainID {
-	case chainIDFantom:
-		headerCh := make(chan *ftmclient.Header)
-		sub, err := c.ftmClient.SubscribeNewHead(ctx, headerCh)
-		if err != nil {
-			return nil, err
+	case chainIDFantom, chainIDAvalanche, chainIDZKSync:
+		var (
+			err      error
+			sub      Subscription
+			headerCh = make(chan *commonclient.Header)
+		)
+
+		if c.chainID == chainIDAvalanche {
+			sub, err = c.avaxClient.SubscribeNewHead(ctx, headerCh)
+		} else {
+			sub, err = c.customClient.SubscribeNewHead(ctx, headerCh)
 		}
-
-		go func() {
-			for {
-				select {
-				case <-ctx.Done():
-					return
-				case header := <-headerCh:
-					ch <- &types.Header{
-						Hash:       common.ToHex(header.Hash),
-						ParentHash: common.ToHex(header.ParentHash),
-						Number:     header.Number,
-						Time:       header.Time,
-					}
-				}
-			}
-		}()
-
-		return sub, nil
-	case chainIDAvalanche:
-		headerCh := make(chan *avaxtypes.Header)
-		sub, err := c.avaxClient.SubscribeNewHead(ctx, headerCh)
-		if err != nil {
-			return nil, err
-		}
-
-		go func() {
-			for {
-				select {
-				case <-ctx.Done():
-					return
-				case header := <-headerCh:
-					ch <- &types.Header{
-						Hash:       common.ToHex(header.Hash()),
-						ParentHash: common.ToHex(header.ParentHash),
-						Number:     header.Number,
-						Time:       header.Time,
-					}
-				}
-			}
-		}()
-
-		return sub, nil
-	case chainIDZKSync:
-		headerCh := make(chan *zksyncclient.Header)
-		sub, err := c.zksyncClient.SubscribeNewHead(ctx, headerCh)
 		if err != nil {
 			return nil, err
 		}
@@ -250,112 +202,29 @@ func (c *Client) SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) 
 	}
 }
 
-//nolint:dupl
-func (c *Client) ftmFilterLogs(ctx context.Context, q FilterQuery) ([]types.Log, error) {
-	logs, err := c.ftmClient.FilterLogs(ctx, toEthereumFilterQuery(q))
-	if err != nil {
-		return nil, err
-	}
-
-	return fromEthereumLogs(logs), nil
-}
-
-//nolint:dupl
-func (c *Client) avaxFilterLogs(ctx context.Context, q FilterQuery) ([]types.Log, error) {
-	var blockHash *ethcommon.Hash
-	if q.BlockHash != nil {
-		hash := ethcommon.HexToHash(*q.BlockHash)
-		blockHash = &hash
-	}
-
-	addresses := make([]ethcommon.Address, 0, len(q.Addresses))
-	for _, address := range q.Addresses {
-		addresses = append(addresses, ethcommon.HexToAddress(address))
-	}
-
-	topics := make([][]ethcommon.Hash, 0, len(q.Topics))
-	for i, ts := range q.Topics {
-		topics[i] = make([]ethcommon.Hash, 0, len(ts))
-		for _, t := range ts {
-			topics[i] = append(topics[i], ethcommon.HexToHash(t))
-		}
-	}
-
-	logs, err := c.avaxClient.FilterLogs(ctx, interfaces.FilterQuery{
-		BlockHash: blockHash,
-		FromBlock: q.FromBlock,
-		ToBlock:   q.ToBlock,
-		Addresses: addresses,
-		Topics:    topics,
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	return fromAvalancheLogs(logs), nil
-}
-
-//nolint:dupl
-func (c *Client) ethFilterLogs(ctx context.Context, q FilterQuery) ([]types.Log, error) {
-	logs, err := c.ethClient.FilterLogs(ctx, toEthereumFilterQuery(q))
-	if err != nil {
-		return nil, err
-	}
-
-	return fromEthereumLogs(logs), nil
-}
-
-//nolint:dupl
-func (c *Client) zksyncFilterLogs(ctx context.Context, q FilterQuery) ([]types.Log, error) {
-	logs, err := c.zksyncClient.FilterLogs(ctx, toEthereumFilterQuery(q))
-	if err != nil {
-		return nil, err
-	}
-
-	return fromEthereumLogs(logs), nil
-}
-
 func (c *Client) FilterLogs(ctx context.Context, q FilterQuery) ([]types.Log, error) {
 	switch c.chainID {
-	case chainIDFantom:
-		return c.ftmFilterLogs(ctx, q)
+	case chainIDFantom, chainIDZKSync:
+		return filterLogs(ctx, c.customClient, q)
 	case chainIDAvalanche:
-		return c.avaxFilterLogs(ctx, q)
-	case chainIDZKSync:
-		return c.zksyncFilterLogs(ctx, q)
+		return filterLogs(ctx, c.avaxClient, q)
 	default:
-		return c.ethFilterLogs(ctx, q)
+		return filterLogs(ctx, c.ethClient, q)
 	}
 }
 
 func (c *Client) HeaderByHash(ctx context.Context, hash string) (*types.Header, error) {
 	switch c.chainID {
-	case chainIDFantom:
-		header, err := c.ftmClient.HeaderByHash(ctx, ethcommon.HexToHash(hash))
-		if err != nil {
-			return nil, err
+	case chainIDFantom, chainIDAvalanche, chainIDZKSync:
+		var (
+			err    error
+			header *commonclient.Header
+		)
+		if c.chainID == chainIDAvalanche {
+			header, err = c.avaxClient.HeaderByHash(ctx, ethcommon.HexToHash(hash))
+		} else {
+			header, err = c.customClient.HeaderByHash(ctx, ethcommon.HexToHash(hash))
 		}
-
-		return &types.Header{
-			Hash:       common.ToHex(header.Hash),
-			ParentHash: common.ToHex(header.ParentHash),
-			Number:     header.Number,
-			Time:       header.Time,
-		}, nil
-	case chainIDAvalanche:
-		header, err := c.avaxClient.HeaderByHash(ctx, ethcommon.HexToHash(hash))
-		if err != nil {
-			return nil, err
-		}
-
-		return &types.Header{
-			Hash:       common.ToHex(header.Hash()),
-			ParentHash: common.ToHex(header.ParentHash),
-			Number:     header.Number,
-			Time:       header.Time,
-		}, nil
-	case chainIDZKSync:
-		header, err := c.zksyncClient.HeaderByHash(ctx, ethcommon.HexToHash(hash))
 		if err != nil {
 			return nil, err
 		}
@@ -383,32 +252,16 @@ func (c *Client) HeaderByHash(ctx context.Context, hash string) (*types.Header, 
 
 func (c *Client) HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error) {
 	switch c.chainID {
-	case chainIDFantom:
-		header, err := c.ftmClient.HeaderByNumber(ctx, number)
-		if err != nil {
-			return nil, err
+	case chainIDFantom, chainIDAvalanche, chainIDZKSync:
+		var (
+			err    error
+			header *commonclient.Header
+		)
+		if c.chainID == chainIDAvalanche {
+			header, err = c.avaxClient.HeaderByNumber(ctx, number)
+		} else {
+			header, err = c.customClient.HeaderByNumber(ctx, number)
 		}
-
-		return &types.Header{
-			Hash:       common.ToHex(header.Hash),
-			ParentHash: common.ToHex(header.ParentHash),
-			Number:     header.Number,
-			Time:       header.Time,
-		}, nil
-	case chainIDAvalanche:
-		header, err := c.avaxClient.HeaderByNumber(ctx, number)
-		if err != nil {
-			return nil, err
-		}
-
-		return &types.Header{
-			Hash:       common.ToHex(header.Hash()),
-			ParentHash: common.ToHex(header.ParentHash),
-			Number:     header.Number,
-			Time:       header.Time,
-		}, nil
-	case chainIDZKSync:
-		header, err := c.zksyncClient.HeaderByNumber(ctx, number)
 		if err != nil {
 			return nil, err
 		}
@@ -432,85 +285,4 @@ func (c *Client) HeaderByNumber(ctx context.Context, number *big.Int) (*types.He
 			Time:       header.Time,
 		}, nil
 	}
-}
-
-func toEthereumFilterQuery(q FilterQuery) ethereum.FilterQuery {
-	var blockHash *ethcommon.Hash
-	if q.BlockHash != nil {
-		hash := ethcommon.HexToHash(*q.BlockHash)
-		blockHash = &hash
-	}
-
-	addresses := make([]ethcommon.Address, 0, len(q.Addresses))
-	for _, address := range q.Addresses {
-		addresses = append(addresses, ethcommon.HexToAddress(address))
-	}
-
-	var topics [][]ethcommon.Hash
-	if len(q.Topics) > 0 {
-		topics = make([][]ethcommon.Hash, 0, len(q.Topics))
-		for _, ts := range q.Topics {
-			tps := make([]ethcommon.Hash, 0, len(ts))
-			for _, t := range ts {
-				tps = append(tps, ethcommon.HexToHash(t))
-			}
-			topics = append(topics, tps)
-		}
-	}
-
-	return ethereum.FilterQuery{
-		BlockHash: blockHash,
-		FromBlock: q.FromBlock,
-		ToBlock:   q.ToBlock,
-		Addresses: addresses,
-		Topics:    topics,
-	}
-}
-
-func fromEthereumLogs(logs []ethtypes.Log) []types.Log {
-	res := make([]types.Log, 0, len(logs))
-	for _, log := range logs {
-		topics := make([]string, 0, len(log.Topics))
-		for _, topic := range log.Topics {
-			topics = append(topics, common.ToHex(topic))
-		}
-
-		res = append(res, types.Log{
-			Address:     common.ToHex(log.Address),
-			Topics:      topics,
-			Data:        log.Data,
-			BlockNumber: log.BlockNumber,
-			TxHash:      common.ToHex(log.TxHash),
-			TxIndex:     log.TxIndex,
-			BlockHash:   common.ToHex(log.BlockHash),
-			Index:       log.Index,
-			Removed:     log.Removed,
-		})
-	}
-
-	return res
-}
-
-func fromAvalancheLogs(logs []avaxtypes.Log) []types.Log {
-	res := make([]types.Log, 0, len(logs))
-	for _, log := range logs {
-		topics := make([]string, 0, len(log.Topics))
-		for _, topic := range log.Topics {
-			topics = append(topics, common.ToHex(topic))
-		}
-
-		res = append(res, types.Log{
-			Address:     common.ToHex(log.Address),
-			Topics:      topics,
-			Data:        log.Data,
-			BlockNumber: log.BlockNumber,
-			TxHash:      common.ToHex(log.TxHash),
-			TxIndex:     log.TxIndex,
-			BlockHash:   common.ToHex(log.BlockHash),
-			Index:       log.Index,
-			Removed:     log.Removed,
-		})
-	}
-
-	return res
 }
